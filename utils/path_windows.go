d5ef840b1a095e70ca295fbd0cb34d5eca830aa1
Robert Tingirica <rtingirica@cloudbasesolutions.com>
2014-06-04 16:56:02 +0300

    Added tests for utils/path_windows + bug fixes

--- utils/path_windows.go
+++ utils/path_windows.go
@@ -40,8 +40,11 @@ func CreateSymLink(link, target string) error {
 		CreateSymbolicLinkW, _ = syscall.GetProcAddress(kernel32, "CreateSymbolicLinkW")
 	)
 	var nargs uintptr = 3
-	_, _, callErr := syscall.Syscall(uintptr(CreateSymbolicLinkW), nargs,
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(link))), uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(target))), uintptr(dwFlag))
+	_, _, callErr := syscall.Syscall(
+		uintptr(CreateSymbolicLinkW), nargs,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(link))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(target))),
+		uintptr(dwFlag))
 	if callErr != 0 {
 		return errors.New(fmt.Sprintf("CreateSymbolicLinkW Error: %v", callErr))
 	}
@@ -50,40 +53,68 @@ func CreateSymLink(link, target string) error {
 }
 
 func Readlink(link string) (string, error) {
-	var (
-		kernel32                  = syscall.NewLazyDLL("kernel32.dll")
-		GetFinalPathNameByHandleW = kernel32.NewProc("GetFinalPathNameByHandleW")
+    var (
+        kernel = syscall.MustLoadDLL("kernel32.dll")
+        CreateFile = kernel.MustFindProc("CreateFileW")
+        GetFinalPathNameByHandleW = kernel.MustFindProc("GetFinalPathNameByHandleW")
 
-		nargs    uint32 = 4
-		buf_size int    = 526
-		buf      [526]byte
-		target   string
-	)
+        buf_size int    = 526
+        buf      [526]byte
+        target  string
+    )
 
-	handle, Err := syscall.Open(link, 0, 2)
-	if Err != nil {
-		return "", errors.New(fmt.Sprintf("CreateFileW Error: %v", Err))
-	}
+    handle, _, callErr := CreateFile.Call(
+        uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(link))), 
+        uintptr(syscall.GENERIC_READ), 
+        syscall.FILE_SHARE_READ,
+        0, 
+        syscall.OPEN_EXISTING, 
+        uintptr(33554432), 
+        0)
+    if callErr.Error() != "The operation completed successfully." {
+        return "", errors.New(fmt.Sprintf("ReadFile Error: %v", callErr))
+    }
 
-	_, _, callErr := syscall.Syscall6(GetFinalPathNameByHandleW.Addr(),
-		uintptr(unsafe.Pointer(&nargs)),
-		uintptr(unsafe.Pointer(handle)),
-		uintptr(unsafe.Pointer(&buf)),
-		uintptr(buf_size), 0, 0, 0)
-	if callErr != 0 {
-		return "", errors.New(fmt.Sprintf("GetFinalPathNameByHandleW Error: %v", callErr))
-	}
+    _, _, callErr = GetFinalPathNameByHandleW.Call(
+        uintptr(unsafe.Pointer(handle)),
+        uintptr(unsafe.Pointer(&buf)),
+        uintptr(buf_size), 0)
+    if callErr.Error() != "The operation completed successfully." {
+        return "", errors.New(fmt.Sprintf("GetFinalPathNameByHandleW Error: %v", callErr))
+    }
 
-	defer syscall.CloseHandle(handle)
-	for i, _ := range buf {
-		if buf[i] != 0 {
-			target += string(buf[i])
-		}
-	}
-	if target[:4] == `\\?\` {
-		target = target[4:]
-	}
-	return target, nil
+    for i, _ := range buf {
+        if buf[i] != 0 {
+            target += string(buf[i])
+        }
+    }
+    if len(target) > 4 {
+        if target[:4] == `\\?\` {
+            target = target[4:]
+        }
+    }
+    return target, nil
+}
+
+func GetLongPath(path string) (string, error){
+	p, err := syscall.UTF16FromString(path)
+	if err != nil {
+		return "", err
+    }
+	b := p
+	n, err := syscall.GetLongPathName(&p[0], &b[0], uint32(len(b)))
+	if err != nil {
+		return "", err
+    }
+	if n > uint32(len(b)) {
+		b = make([]uint16, n)
+		n, err = syscall.GetLongPathName(&p[0], &b[0], uint32(len(b)))
+		if err != nil {
+    		return "", err
+		}		
+    }
+    b = b[:n]
+    return syscall.UTF16ToString(b), nil
 }
 
 func Symlink(oldname, newname string) error {
